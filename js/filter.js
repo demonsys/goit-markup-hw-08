acd415d590aa46efd1c2a325fe7c173d00c081c8
demonsys <112822206+demonsys@users.noreply.github.com>
2023-03-15 00:30:38 +0200

    Added js filter

--- js/filter.js
+++ js/filter.js
@@ -1,5 +1,6 @@
 const filter = document.querySelector('.filter');
-console.log(filter);
+const cardSet = document.querySelector('.card-set');
+const arrayofCards = [...cardSet.children];
 function onFilterClick(e) {
   if (e.target.nodeName !== 'BUTTON') {
     return;
@@ -7,6 +8,21 @@ function onFilterClick(e) {
   const activeEl = filter.querySelector('.filter__button--active');
   activeEl.classList.remove('filter__button--active');
   e.target.classList.add('filter__button--active');
+  createFilterTag();
 }
 
 filter.addEventListener('click', onFilterClick);
+function createFilterTag() {
+  const activeFilter = filter.querySelector('.filter__button--active');
+  const filterTag = activeFilter.dataset.value;
+  renderCardSet(filterTag);
+}
+function renderCardSet(filterTag) {
+  cardSet.innerHTML = '';
+  newSet = arrayofCards.filter(
+    li => li.querySelector('p.project-card__text').textContent === filterTag
+  );
+  if (filterTag === 'Усі') {
+    cardSet.append(...arrayofCards);
+  } else cardSet.append(...newSet);
+}
